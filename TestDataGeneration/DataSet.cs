cd3e50db812cd22082c26e1f0fb3774b2022f1be
Adrian Bunea <adrian_bunea28@yahoo.com>
2019-10-09 23:44:25 +0300

    Add Point generation algorithm

--- TestDataGeneration/DataSet.cs
+++ TestDataGeneration/DataSet.cs
@@ -9,17 +9,66 @@ namespace TestDataGeneration
 {
     class DataSet
     {
+        Random random = new Random();
+        readonly int minX = -400, minY = -400, maxX = 400, maxY = 400;
+        private Point center;
+        private Point dispersion;
         public List<Point> points;
 
-        public DataSet()
+        public DataSet(double mean, double dispersion)
         {
-            points = new List<Point>
+            center = new Point((int)mean, (int)mean);
+            this.dispersion = new Point((int)dispersion, (int)dispersion);
+            points = generateRandomPoints();
+        }
+
+        List<Point> generateRandomPoints()
+        {
+            List<Point> points = new List<Point>();
+            for (int i = 0; i < 1000; i++)
             {
-                new Point(45, 45),
-                new Point(45, -45),
-                new Point(-45, 45),
-                new Point(-45, -45),
-            };
+                int x = generateX();
+                int y = generateY();
+                points.Add(new Point(x, y));
+            }
+
+            return points;
+        }
+
+        int generateX()
+        {
+            int x;
+            while (true)
+            {
+                x = random.Next(minX, maxX);
+                double gauss = Gauss(x, center.X, dispersion.X);
+                double odds = random.NextDouble();
+                if (odds < gauss) break;
+            }
+            
+            return x;
+        }
+
+        int generateY()
+        {
+            int y;
+            while (true)
+            {
+                y = random.Next(minY, maxY);
+                double gauss = Gauss(y, center.Y, dispersion.Y);
+                double odds = random.NextDouble();
+                if (odds < gauss) break;
+            }
+
+            return y;
+        }
+
+        double Gauss(int value, double mean, double dispersion)
+        {
+            double delta = mean - value;
+            double variance = Math.Pow(dispersion, 2);
+            double power = Math.Pow(delta, 2) / (2 * variance);
+            return Math.Pow(Math.E, -power);
         }
     }
 }
--- TestDataGeneration/Form1.cs
+++ TestDataGeneration/Form1.cs
@@ -7,25 +7,35 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using System.Windows.Forms.DataVisualization.Charting;
 
 namespace TestDataGeneration
 {
     public partial class Form1 : Form
     {
-        private DataSet[] dataSets = new DataSet[5]
+        private DataSet[] dataSets = new DataSet[3]
         {
-            new DataSet(),
-            new DataSet(),
-            new DataSet(),
-            new DataSet(),
-            new DataSet()
+            new DataSet(0, 50),
+            new DataSet(240, 50),
+            new DataSet(-290, 50)
         };
 
         public Form1()
         {
             InitializeComponent();
 
-            chart.Series["Series1"].Points.DataBind(dataSets[0].points, "X", "Y", null);
+            chart.Series.Clear();
+            foreach(DataSet dataSet in dataSets)
+            {
+                Series series = new Series();
+                series.Points.DataBind(dataSet.points, "X", "Y", null);
+                series.ChartType = SeriesChartType.Point;
+                series.BorderColor = Color.Transparent;
+                series.MarkerSize = 5;
+                series.CustomProperties = "IsXAxisQuantitative=True";
+
+                chart.Series.Add(series);
+            }
         }
     }
 }
